9bb47025307631a4190615d5e5f1cf6269dfdf36
ekeerthana01-ui <ekeerthana01@gmail.com>
2026-06-24 23:37:27 +0530

    Fix logout redirect issue

--- pms/urls.py
+++ pms/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
 
     # 🔥 THIS MUST BE HERE
     path('signup/', views.signup, name='signup'),
